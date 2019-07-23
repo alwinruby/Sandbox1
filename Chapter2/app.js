9e81016f148f27dc5673ea9903aab83ee5c205c1
asolanky <alwinsolanky@gmail.com>
2019-07-23 14:38:18 +0100

    static methods

--- Chapter2/app.js
+++ Chapter2/app.js
@@ -10,13 +10,25 @@ class Car {
   carStats(){
     return `This car has ${this.doors} doors, a ${this.engine} and a beautiful ${this.colour} colour!`;
   }
+
+  static totalDoors(car1, car2) {
+    const doors1 = car1.doors;
+    const doors2 = car2.doors;
+
+    return doors1 + doors2;
+
+  }
+
 }
 
 const cx5 = new Car( 4, 'V6', 'green' )
+const civic = new Car( 3, 'V4', 'red' )
 
 console.log(cx5);
-
 console.log(cx5.carStats());
+console.log(civic);
+console.log(civic.carStats());
+console.log(Car.totalDoors(cx5, civic));
 
 function sayHi(){
   return console.log('This function can be called from anywhere!');
--- README.md
+++ README.md
@@ -6,10 +6,12 @@
 
 ###CLASSES
 
-    __NOT__ Hoisted cant be called anywhere, __CANNOT__ be overwritten
+    __NOT__ Hoisted cant be called anywhere, __CANNOT__ be overwritten.
 
 
 
 Build something in React so you are continuously being challenged about building a stateless or stateful component.
 
 Create a class and add methods for reuse.
+
+When you declare a class its automatically in strict mode, this was introduced in ES5.
